24ea6161e2bf64eb4a0fd25dea0766ebb77768b4
tavoGeek <tavogeek@github.com>
2019-08-13 18:50:08 -0500

    se creo la cabecera del menu

--- app/src/main/java/com/examples/aguadulce/menu/Menu.java
+++ app/src/main/java/com/examples/aguadulce/menu/Menu.java
@@ -3,14 +3,22 @@ package com.examples.aguadulce.menu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.examples.aguadulce.R;
 
 public class Menu extends AppCompatActivity {
 
+    private TextView tvBienvenido;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        tvBienvenido = findViewById(R.id.tvBienvenido);
+
+        String mensajeBienvenida = tvBienvenido.getText().toString() + "\nGustavo Castillo Nájera";
+        tvBienvenido.setText(mensajeBienvenida);
     }
 }
